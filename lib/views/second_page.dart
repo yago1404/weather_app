cb782ca42e22d3802f1e9d521e3effbc9eb159a0
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2022-05-11 18:38:31 -0300

    Update second_page.dart

--- lib/views/second_page.dart
+++ lib/views/second_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:weather_app/models/weather.dart';
 
 import '../providers/weather_provider.dart';
 
